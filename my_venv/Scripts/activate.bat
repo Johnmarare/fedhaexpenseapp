0083bbd2af66e033eb139e9d7e714eacc588b1d6
Johnmarare <mararejohn@gmail.com>
2024-09-06 21:16:33 +0300

    file tree change

--- my_venv/Scripts/activate.bat
+++ my_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\OneDrive\Desktop\Django\my_venv
+set VIRTUAL_ENV=C:\Users\Admin\OneDrive\Desktop\Fedha\my_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
