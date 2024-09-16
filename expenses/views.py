f17071a2ffb247485dec0263ec03f2151e3432f3
Johnmarare <mararejohn@gmail.com>
2024-09-16 16:49:43 +0300

    paginated template

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
 from .models import Category, Expense
 # Create your views here.
 
@@ -8,9 +9,12 @@ from .models import Category, Expense
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-
+    paginator = Paginator(expenses, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'expenses': expenses
+        'expenses': expenses,
+        'page_obj': page_obj
     }
     return render(request, 'expenses/index.html', context)
 
